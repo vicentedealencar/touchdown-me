58466a4510d7e4cec789b58fb139939b3607084b
Vicente de Alencar <vicentedealencar@gmail.com>
2012-10-17 03:07:24 -0300

    Agent Hello World

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import madkit.kernel.Madkit;
 
 public class Main {
-
 	public static void main(String[] args) {
 		System.out.println("I`m not a agent...");
+		System.out.println("BUT HE IS!!!");
+		String[] args2 = { "--launchAgents",
+				"agents.FirstAgent,true" }; // This agent with GUI
+		Madkit.main(args2);
 	}
-
 }
