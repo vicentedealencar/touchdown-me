3f66155656efd9a1271cb7c110640fddb926097e
Vicente de Alencar <vicentedealencar@gmail.com>
2012-12-11 11:45:49 -0200

    offensive player runs

--- js/offense.js
+++ js/offense.js
@@ -4,9 +4,9 @@ function Offense(opt_options) {
         world = Flora.universe.first();
 
     Flora.Element.call(this, options);
-    this.acceleration = new Flora.Vector(0, 0.1);
-    this.velocity = new Flora.Vector();
-    this.location = new Flora.Vector(world.width / 2, 0);
+    this.accel = 0.1;
+    this.vel = 0;
+    this.location = new Flora.Vector(world.width / 2, world.height / 2);
     this.hasBall = !!options.hasBall;
 }
 
@@ -14,13 +14,28 @@ exports.Utils.extend(Offense, Flora.Element);
 Offense.prototype.name = 'Offense';
 
 Offense.prototype.step = function () {
-    var world = Flora.universe.first();
 
-    this.velocity.add(this.acceleration);
-    this.location.add(this.velocity);
+    //soma vetores dos def
+    var sum = { x: 0, y: 0 };
+    var anyPlayer = false;
+    var self = this;
+    play.defensivePlayers
+        .filter(function (each) {
+            return each.vel > 1;
+        })
+        .forEach(function (each) {
+            anyPlayer = true;
+            sum.x -= self.location.x - each.location.x
+            sum.y -= self.location.y - each.location.y
+        });
 
-    if (this.location.y + this.height / 2 > world.height) {
-        this.velocity.mult(-1);
-        this.location.y = world.height - this.height / 2;
+    if (play.isOver) return;
+
+    if (anyPlayer){
+        var angle = Math.atan2(sum.x, sum.y);
+
+        this.vel += this.accel;
+        this.location.x += this.vel * Math.sin(angle) * -1;
+        this.location.y += this.vel * Math.cos(angle) * -1;
     }
 };
